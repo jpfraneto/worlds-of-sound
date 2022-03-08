61c0b4760b6f418f34caffad7d02627106d604cb
jpfraneto <jpfraneto@gmail.com>
2022-03-08 06:34:00 -0300

    new deploy

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 import MainLandingSection from '../components/MainLandingSection';
-import { useSession, signIn, signOut } from 'next-auth/react';
 
 export default function Home({ scheduledDays }) {
-  const { data: session } = useSession();
   return (
     <div className={styles.container}>
       <Head>
@@ -23,7 +19,7 @@ export default function Home({ scheduledDays }) {
 }
 
 export async function getServerSideProps() {
-  let dev = process.env.NODE_ENV !== 'production';
+  let dev = process.env.VERCEL_ENV !== 'production';
   let { DEV_URL, PROD_URL } = process.env;
   const response = await fetch(
     `${dev ? DEV_URL : PROD_URL}/api/sounds/schedule`
